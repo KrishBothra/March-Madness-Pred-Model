217cf8150d33f35e1d2bbe7127b1361cbfd1d9e7
DumplingDefender <messiharden1@gmail.com>
2026-03-03 18:49:31 -0600

    matchups DF

--- pred-model.R
+++ pred-model.R
@@ -1,28 +1,60 @@
 library(tidyverse)
+library(ggthemes)
+library(ranger)
+library(vip)
+library(caret)
+library(xgboost)
 library(dplyr)
 
-
 # 1. Set the path to your folder
+
 folder_path <- "March-Madness/march-madness-data"
-
-# 2. Get a list of all the CSV file paths in that folder
 file_paths <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
-
-# 3. Extract just the file names (without the ".csv" part)
 file_names <- tools::file_path_sans_ext(basename(file_paths))
-
-# 4. Clean up the names (R doesn't like spaces or starting with numbers)
-# This changes "538 Ratings" to "X538_Ratings" and "AP Poll Data" to "AP_Poll_Data"
 clean_names <- gsub("[ -]", "_", file_names)
 clean_names <- make.names(clean_names) 
-
-# 5. Read all files into a single list of data frames using base R
-# (If your files are huge, you can swap read.csv with readr::read_csv for speed)
 my_data_list <- lapply(file_paths, read.csv)
-
-# 6. Apply your clean names to the list
 names(my_data_list) <- clean_names
-
-# 7. Unpack the list into your Global Environment
-# Now you will see them all pop up in your top-right RStudio pane!
 list2env(my_data_list, envir = .GlobalEnv)
+
+
+matchups <- read.csv("March-Madness/march-madness-data/Tournament Matchups.csv", check.names = FALSE)
+
+Barttorvik_Away |> 
+  select(TEAM)
+
+matchups <- matchups |> 
+  mutate(
+    Game_ID = (row_number() + 1) %/% 2,
+    Team_Label = rep(c("A","B"), length.out = n())
+  ) |> 
+  select(-`BY YEAR NO`, -`TEAM NO`) |> 
+  pivot_wider(
+    id_cols = c(Game_ID, YEAR, `CURRENT ROUND`),
+    names_from = Team_Label,
+    values_from = c(TEAM, SEED, SCORE, ROUND)
+  ) |> 
+  mutate(
+    Winner = ifelse(SCORE_A > SCORE_B, TEAM_A, TEAM_B),
+    Loser  = ifelse(SCORE_A > SCORE_B, TEAM_B, TEAM_A)
+  )
+
+matchups<-matchups |> 
+  mutate(
+    hSeedTeam = ifelse(SEED_A <= SEED_B, TEAM_A, TEAM_B),
+    lSeedTeam = ifelse(SEED_A <= SEED_B, TEAM_B, TEAM_A),
+    
+    hSeed        = ifelse(SEED_A <= SEED_B, SEED_A, SEED_B),
+    lSeed         = ifelse(SEED_A <= SEED_B, SEED_B, SEED_A),
+    
+    hSeedScore  = ifelse(SEED_A <= SEED_B, SCORE_A, SCORE_B),
+    lSeedScore  = ifelse(SEED_A <= SEED_B, SCORE_B, SCORE_A)
+  ) |> 
+  select(-TEAM_A, -TEAM_B, -SEED_A, -SEED_B, -SCORE_A, -SCORE_B)
+
+matchups |> 
+  select(TEAM)
+
+
+
+
